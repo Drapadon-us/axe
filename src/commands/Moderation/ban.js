177fadb2a5bab2325033118f0ba7a6b6119f0abe
drapadon <extramath842@gmail.com>
2025-11-04 11:35:04 -0800

    Fixed unknown interaction error in logs
    interaction.deferReply() now acknowledges the interaction immediately

--- src/commands/Moderation/ban.js
+++ src/commands/Moderation/ban.js
@@ -1,68 +1,54 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { EmbedBuilder, PermissionsBitField } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, PermissionsBitField } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('ban')
-        .setDescription('Bans a user')
-        .addUserOption(option =>
-            option.setName('target')
-                .setDescription('The user you would like to ban')
-                .setRequired(true)
-        )
-        .addStringOption(option =>
-            option.setName('reason')
-                .setDescription('The reason for banning the user')
-        ),
+    .setName('ban')
+    .setDescription('Bans a user')
+    .addUserOption(option =>
+    option.setName('target')
+    .setDescription('The user you would like to ban')
+    .setRequired(true)
+    )
+    .addStringOption(option =>
+    option.setName('reason')
+    .setDescription('The reason for banning the user')
+    ),
 
-    async execute(interaction, client) {
+    async execute(interaction) {
+        await interaction.deferReply({ ephemeral: false }); // <-- acknowledges interaction early
+
         const banUser = interaction.options.getUser('target');
         const banMember = await interaction.guild.members.fetch(banUser.id).catch(() => null);
-        const channel = interaction.channel;
 
         if (!interaction.member.permissions.has(PermissionsBitField.Flags.BanMembers)) {
-            return await interaction.reply({
-                content: "You dont have the **Ban Members** permission.",
-                ephemeral: true
-            });
+            return await interaction.editReply("You don't have the **Ban Members** permission.");
         }
 
         if (!banMember) {
-            return await interaction.reply({
-                content: 'That user is NOT in the server',
-                ephemeral: true
-            });
+            return await interaction.editReply("That user is NOT in the server.");
         }
 
         if (!banMember.bannable) {
-            return await interaction.reply({
-                content: "I can't ban this user because their role is above me or they have higher permissions.",
-                ephemeral: true
-            });
+            return await interaction.editReply("I can't ban this user because their role is above me or they have higher permissions.");
         }
 
-        let reason = interaction.options.getString('reason');
-        if (!reason) reason = "No reason provided";
+        let reason = interaction.options.getString('reason') || "No reason provided";
 
         const dmEmbed = new EmbedBuilder()
-            .setColor("Red")
-            .setDescription(`You've have been banned from **${interaction.guild.name}**\n**reason:** ${reason}`);
+        .setColor("Red")
+        .setDescription(`You've been banned from **${interaction.guild.name}**\n**Reason:** ${reason}`);
 
         const embed = new EmbedBuilder()
-            .setColor("Red")
-            .setDescription(`I wiped **${banUser.tag}** from the face of this server \n**Reason:** ${reason}`);
+        .setColor("Red")
+        .setDescription(` **${banUser.tag}** was banned.\n**Reason:** ${reason}`);
 
-        await banUser.send({ embeds: [dmEmbed] }).catch(err => {
-            // doesn't attempt to send a dm if users dms are closed
-        });
-
-        await banMember.ban({ reason: reason }).catch(err => {
-            return interaction.reply({
-                content: "I don't know why, but I can't ban this user.",
-                ephemeral: true
-            });
-        });
-
-        await interaction.reply({ embeds: [embed] });
+        try {
+            await banUser.send({ embeds: [dmEmbed] }).catch(() => {});
+            await banMember.ban({ reason });
+            await interaction.editReply({ embeds: [embed] });
+        } catch (err) {
+            console.error(err);
+            await interaction.editReply("I can't ban this user. Please check my permissions and role position.");
+        }
     }
-}
+};
